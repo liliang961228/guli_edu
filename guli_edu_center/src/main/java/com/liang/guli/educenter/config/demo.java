e3c45f9bc2dd8e4cb7afb28bf9f79233fb198f31
liliang <1254842504@qq.com>
2019-06-03 13:07:27 +0800

    修改demo第一次

--- guli_edu_center/src/main/java/com/liang/guli/educenter/config/demo.java
+++ guli_edu_center/src/main/java/com/liang/guli/educenter/config/demo.java
@@ -4,5 +4,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hhaha");
         System.out.println("hhaha");
+        System.out.println("hhaha");
+
     }
 }
